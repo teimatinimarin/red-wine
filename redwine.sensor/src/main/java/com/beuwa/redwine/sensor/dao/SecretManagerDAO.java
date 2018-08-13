45be5a0c9831d978365e1d9873704258ac4ffc31
teimatinim <teimatini.marin@fox.com>
2018-08-13 13:33:05 -0500

    red-wine-10 Introduce jigsaw directory/module conventions gradle does not implement native support for jigsaw yet. Some issues were found with jigsaw + slf4j + junit

---
+++ redwine.sensor/src/main/java/com/beuwa/redwine/sensor/dao/SecretManagerDAO.java
@@ -0,0 +1,29 @@
+package com.beuwa.redwine.sensor.dao;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+
+public class SecretManagerDAO {
+    public static void main(String[] args) {
+        SecretsManagerClient smClient = SecretsManagerClient.builder()
+                .region(Region.EU_WEST_1)
+                .build();
+
+        // In case of Certificate Issues, see
+        // https://stackoverflow.com/questions/6784463/error-trustanchors-parameter-must-be-non-empty/30699960#30699960
+        GetSecretValueResponse response = smClient.getSecretValue( GetSecretValueRequest.builder()
+                .secretId("test")
+                .build() );
+
+        JsonReader jsonReader = Json.createReader( new StringReader(response.secretString()) );
+        JsonObject jsonObject = jsonReader.readObject();
+        System.out.println( jsonObject.getString("endpoint") );
+    }
+}
---
+++ redwine.sensor/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module redwine.sensor {
+    // JSON
+    requires org.glassfish.java.json;
+
+    // Websocket Client
+    requires tyrus.client;
+    requires javax.websocket.api;
+
+    // Logging
+    requires org.apache.logging.log4j;
+
+    // AWS
+    requires aws.core;
+    requires secretsmanager;
+    requires regions;
+
+    exports com.beuwa.redwine.sensor;
+    opens com.beuwa.redwine.sensor.config to org.junit.platform.commons;
+
+}
