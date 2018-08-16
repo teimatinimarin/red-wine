b7c6e7a6715afca91d9223d66cca29faed6e882e
teimatinim <teimatini.marin@fox.com>
2018-08-16 15:58:56 -0500

    red-wine-9 Get properties from AWS Secrets Manager

---
+++ redwine.sensor/src/main/java/com/beuwa/redwine/sensor/config/PropertiesFactory.java
@@ -0,0 +1,24 @@
+package com.beuwa.redwine.sensor.config;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+
+public class PropertiesFactory {
+    public static Properties createProperties(String secrets) {
+        JsonReader jsonReader = Json.createReader( new StringReader(secrets) );
+        JsonObject jsonObject = jsonReader.readObject();
+
+        return new Properties.PropertiesBuilder()
+                .endpoint(    jsonObject.getString("endpoint") )
+                .apiKey(      jsonObject.getString("api_key") )
+                .apiSecret(   jsonObject.getString("api_secret") )
+                .trading(     jsonObject.getString("trading") )
+                .leverage(    jsonObject.getString("leverage") )
+                .notifyOpen(  jsonObject.getString("open_email") )
+                .notifyClose( jsonObject.getString("close_email") )
+                .maxInvest(   jsonObject.getString("max_invest") )
+                .build();
+    }
+}
